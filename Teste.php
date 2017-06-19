a416ebbc06c06d91a874f790be19c812e4f688bc
Will Xavier <willyelnsg.x@gmail.com>
2017-06-18 21:46:37 -0300

    Fixing github page

--- Teste.php
+++ Teste.php
@@ -6,3 +6,25 @@ class Teste extends PHPUnit_Framework_TestCase
   	}
 }
 ?>
+
+<script>
+    var rotas = [
+        {direto:[
+            {horarioSaida: "10:00", empresa: "Guanabara"},
+            {horarioSaida: "10:00", empresa: "Guanabara"},
+            {horarioSaida: "10:00", empresa: "Guanabara"},
+            {horarioSaida: "10:00", empresa: "Guanabara"},
+            {horarioSaida: "10:00", empresa: "Guanabara"},
+            {horarioSaida: "10:00", empresa: "Guanabara"},
+            {horarioSaida: "10:00", empresa: "Guanabara"}
+        ]},
+        {inverso:[
+            {horarioSaida:"9:30", empresa: "Via Sul"},
+            {horarioSaida:"9:30", empresa: "Via Sul"},
+            {horarioSaida:"9:30", empresa: "Via Sul"},
+            {horarioSaida:"9:30", empresa: "Via Sul"},
+            {horarioSaida:"9:30", empresa: "Via Sul"},
+            {horarioSaida:"9:30", empresa: "Via Sul"}
+        ]}
+    ];
+</script>
