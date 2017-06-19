b276478ffa7d847c5ff9fddfac0f2b3898452ecf
Nayana Farias <naayfarias@hotmail.com>
2017-06-19 14:20:58 -0300

    issue #09 horarios

---
+++ src/web/Controller/BusController.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: raevix
+ * Date: 6/19/17
+ * Time: 12:37 AM
+ */
+require_once 'Bus.php';
+class BusController
+{
+    function verifyNextDir(Bus $bus):array{
+        return array_search($bus->prevDeparture,array_keys($bus->$route))
+    }
+}
